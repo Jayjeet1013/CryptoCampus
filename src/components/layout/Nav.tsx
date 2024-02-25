3b9b69919311f12307f9a3581157e12285818319
Jayjeet1013 <jayjeet1013@gmail.com>
2024-02-25 20:29:50 +0530

    change some colors

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -4,31 +4,38 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className="max-w-[1450px]  mx-auto px-10 ">
-      <div className="flex justify-between py-4 ">
-        <div className="flex items-center ">
-          <Link href={"/"}>
-            <Image src={"/koinxlogo.svg"} width={96} height={24} alt="logo " />
-          </Link>
-        </div>
-        <div className="flex gap-12 ">
-          <div className="flex gap-12 items-center">
+    <div className="bg-white ">
+      <div className="max-w-[1450px]  mx-auto px-10 ">
+        <div className="flex justify-between py-4 ">
+          <div className="flex items-center ">
             <Link href={"/"}>
-              <h1>Crypto Taxes</h1>
-            </Link>
-            <Link href={"/"}>
-              <h1> Free Tools</h1>
-            </Link>
-            <Link href={"/"}>
-              <h1>Resource Center</h1>
+              <Image
+                src={"/koinxlogo.svg"}
+                width={96}
+                height={24}
+                alt="logo "
+              />
             </Link>
           </div>
-          <div>
-            <Link href={"/"}>
-              <button className="bg-[#2870EA] text-[#FFFFFF] px-6 py-2 rounded-lg ">
-                Get Started
-              </button>
-            </Link>
+          <div className="flex gap-12 ">
+            <div className="flex gap-12 items-center">
+              <Link href={"/"}>
+                <h1>Crypto Taxes</h1>
+              </Link>
+              <Link href={"/"}>
+                <h1> Free Tools</h1>
+              </Link>
+              <Link href={"/"}>
+                <h1>Resource Center</h1>
+              </Link>
+            </div>
+            <div>
+              <Link href={"/"}>
+                <button className="bg-[#2870EA] text-[#FFFFFF] px-6 py-2 rounded-lg ">
+                  Get Started
+                </button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,28 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import CryptoInfo from "@/components/home/CryptoInfo";
 import GetStarted from "@/components/home/GetStarted";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-  <div className="flex flex-col gap-5 lg:flex-row px-10 max-w-[1450px] mx-auto justify-between ">
-    <CryptoInfo/>
-    <GetStarted/>
-  </div>
+    <>
+      <Head>
+        <title>KoinX</title>
+        <meta name="KoinX" content="Koinx" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/koinxlogo.svg" />
+      </Head>
+      <div className="bg-[#D3E0E6] ">
+        <div className="max-w-[1450px] mx-auto px-10 py-3 ">
+          Cryptocurrencies {">>"} Bitcoin{" "}
+        </div>
+        <div className="flex flex-col gap-5 lg:flex-row px-10 max-w-[1450px] mx-auto justify-between pt-3 pb-16 ">
+          <CryptoInfo />
+          <GetStarted />
+        </div>
+      </div>
+    </>
   );
 }
