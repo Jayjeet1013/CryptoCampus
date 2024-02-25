5c793d725495fd88af745e0e8c45db4c2f381274
Jayjeet1013 <jayjeet1013@gmail.com>
2024-02-25 19:58:22 +0530

    completed almost aboutcrypto section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-  <div className="flex flex-col lg:flex-row px-10 max-w-[1450px] mx-auto justify-between ">
+  <div className="flex flex-col gap-5 lg:flex-row px-10 max-w-[1450px] mx-auto justify-between ">
     <CryptoInfo/>
     <GetStarted/>
   </div>
