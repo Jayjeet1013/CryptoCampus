fdd10fe1bb8d99eddb1b193e333635d6508049a2
Jayjeet1013 <jayjeet1013@gmail.com>
2024-02-26 14:35:41 +0530

    done top3crypto section

--- src/components/cryptoInfo/Top3Crypto.tsx
+++ src/components/cryptoInfo/Top3Crypto.tsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const Top3Crypto = () => {
-  return (
-    <div>Top3Crypto</div>
-  )
+interface Coin {
+  id: string;
+  name: string;
+  large: string;
+  symbol: string;
+  data: {
+    price_change_percentage_24h?: {
+      usd: number;
+    };
+  };
 }
 
-export default Top3Crypto
+const Top3Crypto: React.FC = () => {
+  const [top3Coins, setTop3Coins] = useState<Coin[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://api.coingecko.com/api/v3/search/trending"
+        );
+        const data = await response.json();
+        setTop3Coins(data.coins.slice(0, 3).map((coin: any) => coin.item));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="py-4 px-6 bg-[#FFFFFF] rounded-lg mt-4 ">
+      <div className="text-[24px] font-semibold mb-2 ">
+        Trending Coins (24h)
+      </div>
+      {top3Coins.map((crypto) => (
+        <div key={crypto.id} className="items-center justify-between flex ">
+          <div className="flex gap-1 items-center">
+            <img
+              src={crypto.large}
+              alt={crypto.name}
+              className="mx-auto"
+              style={{ width: "24px", height: "24px" }}
+            />
+            <h2 className="text-[16px] font-semibold ">
+              {crypto.name}({crypto.symbol}){" "}
+            </h2>
+          </div>
+          {crypto.data.price_change_percentage_24h ? (
+            <p
+              className={`mt-2 text-[16px] ${
+                crypto.data.price_change_percentage_24h.usd >= 0
+                  ? "text-green-500"
+                  : "text-red-500"
+              }`}
+            >
+              {crypto.data.price_change_percentage_24h.usd.toFixed(2)}%
+            </p>
+          ) : (
+            <p className="mt-2 text-lg">Price change data not available</p>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Top3Crypto;
