3d9ed3f8282a4dd7fe14e0928fb0cdc146ce4721
Jayjeet1013 <jayjeet1013@gmail.com>
2024-02-26 20:06:49 +0530

    update navbar

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,40 +1,62 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Nav = () => {
+  const [menu, setMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setMenu(!menu);
+  };
+
   return (
-    <div className="bg-white ">
-      <div className="max-w-[1450px]  mx-auto px-10 ">
-        <div className="flex justify-between py-4 ">
-          <div className="flex items-center ">
+    <div className="bg-white">
+      <div className="max-w-[1450px] mx-auto px-7 md:px-10 relative">
+        <div className="flex justify-between py-4">
+          {/* Logo */}
+          <div className="flex items-center">
             <Link href={"/"}>
-              <Image
-                src={"/koinxlogo.svg"}
-                width={96}
-                height={24}
-                alt="logo "
-              />
+              <Image src={"/koinxlogo.svg"} width={96} height={24} alt="logo" />
             </Link>
           </div>
-          <div className="flex gap-12 ">
-            <div className="flex gap-12 items-center">
+
+          {/* Menu Icon */}
+          <div className="md:hidden">
+            <div onClick={toggleMenu}>
+              {menu ? (
+                <AiOutlineClose style={{ color: "black", fontSize: 32 }} />
+              ) : (
+                <AiOutlineMenu style={{ color: "black", fontSize: 32 }} />
+              )}
+            </div>
+          </div>
+
+          {/* Navigation Links */}
+          <div
+            className={`md:flex gap-12  items-center ${
+              menu ? "absolute  left-0 top-full py-6 w-full bg-white" : "hidden"
+            }`}
+          >
+            <div className="flex md:flex-row flex-col gap-5 md:gap-12 items-center text-[#0F1629] font-semibold text-[16px] ">
               <Link href={"/"}>
                 <h1>Crypto Taxes</h1>
               </Link>
               <Link href={"/"}>
-                <h1> Free Tools</h1>
+                <h1>Free Tools</h1>
               </Link>
               <Link href={"/"}>
                 <h1>Resource Center</h1>
               </Link>
-            </div>
-            <div>
-              <Link href={"/"}>
-                <button className="bg-[#2870EA] text-[#FFFFFF] px-6 py-2 rounded-lg ">
-                  Get Started
-                </button>
-              </Link>
+
+              {/* Get Started Button */}
+              <div>
+                <Link href={"/"}>
+                  <button className="bg-[#2870EA] text-[#FFFFFF] px-6 py-2 rounded-lg">
+                    Get Started
+                  </button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
