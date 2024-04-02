07d8c1be6233748f2409f63de5082f0235f32b9e
Jayjeet1013 <jayjeet1013@gmail.com>
2024-04-02 17:03:46 +0530

    update

--- src/components/cryptoInfo/LiveData.tsx
+++ src/components/cryptoInfo/LiveData.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 interface CoinData {
@@ -6,7 +7,7 @@ interface CoinData {
   name: string;
   image: string;
   current_price: number;
-  // Add other properties as needed
+ 
 }
 
 const LiveData: React.FC = () => {
@@ -33,10 +34,12 @@ const LiveData: React.FC = () => {
     <div className="bg-[#FFFFFF] flex flex-wrap gap-12 p-3 md:p-6 rounded-lg">
       {coinsData.map((coin) => (
         <div key={coin.id}>
+          <Link href={coin.id}>
           <img src={coin.image} className="w-[30px] " alt={coin.name} />
           <p>{coin.name}</p>
           <p>Price: ${coin.current_price}</p>
-          {/* Render other properties as needed */}
+          </Link>
+         
         </div>
       ))}
     </div>
