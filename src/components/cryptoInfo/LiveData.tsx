92923874904fb00af98b314c11d5c1a3a78f4cc9
Jayjeet1013 <jayjeet1013@gmail.com>
2024-04-24 21:41:06 +0530

    udpate

--- src/components/cryptoInfo/LiveData.tsx
+++ src/components/cryptoInfo/LiveData.tsx
@@ -1,4 +1,3 @@
-
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
@@ -12,18 +11,28 @@ interface CoinData {
 
 const LiveData: React.FC = () => {
   const [coinsData, setCoinsData] = useState<CoinData[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
+      setLoading(true);
       try {
         const response = await fetch(
           "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en"
         );
+        if (response.status === 429) {
+          setError(
+            "API limit exceeded, please wait for a few minutes and try again."
+          );
+          return;
+        }
         const data = await response.json();
-      
         setCoinsData(data);
       } catch (error) {
         console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -31,16 +40,22 @@ const LiveData: React.FC = () => {
   }, []);
 
   return (
-    <div className="bg-[#FFFFFF] flex flex-wrap gap-12 p-3 md:p-6 rounded-lg">
-      {coinsData.map((coin) => (
-        <div key={coin.id}>
-          <Link href={`/${coin.id}`}>
-          <img src={coin.image} className="w-[30px] " alt={coin.name} />
-          <p>{coin.name}</p>
-          <p>Price: ${coin.current_price}</p>
-          </Link>
-        </div>
-      ))}
+    <div className="bg-[#FFFFFF] text-black flex flex-wrap gap-12 p-3 md:p-6 rounded-lg">
+      {loading ? (
+        <div>Loading...</div>
+      ) : error ? (
+        <div>{error}</div>
+      ) : (
+        coinsData.map((coin) => (
+          <div key={coin.id} >
+            <Link href={`/${coin.id}`}>
+              <img src={coin.image} className="w-[30px] " alt={coin.name} />
+              <p>{coin.name}</p>
+              <p>Price: ${coin.current_price}</p>
+            </Link>
+          </div>
+        ))
+      )}
     </div>
   );
 };
