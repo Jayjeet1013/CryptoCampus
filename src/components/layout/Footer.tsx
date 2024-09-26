3f7008d33a818adab58df99111a07e5ea889e13f
Jayjeet1013 <jayjeet1013@gmail.com>
2024-09-26 13:31:56 +0530

    fix bugs

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <div className="bg-white border-t-2 text-black py-8 md:py-16 text-left relative overflow-hidden">
@@ -5,9 +7,7 @@ const Footer = () => {
         <div className="md:flex md:flex-row md:justify-between">
           <div className="md:mt-0 md:flex-0 md:w-full md:mb-0 md:flex md:flex-col">
             <div className="flex flex-col gap-1">
-              <a href="/">
-               CryptoZone
-              </a>
+              <Link href="/">CryptoZone</Link>
               <p className="text-xs md:text-sm font-normal">
                 Secondary Marketplace for Real World Assets
               </p>
@@ -19,94 +19,98 @@ const Footer = () => {
               <p className="text-base md:text-lg font-medium mb-2 md:mb-4">
                 About
               </p>
-              <a
+              <Link
                 href="https://asvalabs.com/"
                 target="_blank"
                 className="text-sm md:text-base opacity-50 mb-4"
               >
                 AsvaLabs
-              </a>
-              <a
+              </Link>
+              <Link
                 href={`mailto:contact@asvalabs.com`}
                 className="text-sm md:text-base opacity-50"
                 target="_blank"
               >
                 Contact Us
-              </a>
+              </Link>
             </div>
             <div className="flex flex-col mb-5 md:mb-0">
               <p className="text-base md:text-lg font-medium mb-2 md:mb-4">
                 General
               </p>
-              <a
+              <Link
                 href="#buyers"
                 className="text-sm md:text-base opacity-50 mb-2"
               >
                 Buyers
-              </a>
-              <a href="#asset" className="text-sm md:text-base opacity-50 mb-2">
+              </Link>
+              <Link
+                href="#asset"
+                className="text-sm md:text-base opacity-50 mb-2"
+              >
                 Asset Owners
-              </a>
-              <a
+              </Link>
+              <Link
                 href="https://docs.google.com/forms/d/e/1FAIpQLSfWIJ9wQpTP6IKLhzO25v4mFw276r24AXnYhIO-QbD-H6T-nA/viewform"
                 className="text-sm md:text-base opacity-50"
                 target="_blank"
               >
                 Join Waitlist
-              </a>
+              </Link>
             </div>
             <div className="flex flex-col mb-5 md:mb-0">
               <p className="text-base md:text-lg font-medium mb-2 md:mb-4">
                 Socials
               </p>
-              <a
+              <Link
                 href="https://twitter.com/Tangidocom"
                 className="text-sm md:text-base opacity-50 mb-2"
                 target="_blank"
               >
                 Twitter
-              </a>
-              <a
+              </Link>
+              <Link
                 href="https://t.me/AsvaLabsOfficial"
                 className="text-sm md:text-base opacity-50 mb-2"
                 target="_blank"
               >
                 Telegram
-              </a>
-              <a
+              </Link>
+              <Link
                 href="https://www.linkedin.com/company/asvalabsofficial/"
                 className="text-sm md:text-base opacity-50 mb-2"
                 target="_blank"
               >
                 Linkedin
-              </a>
-              <a
+              </Link>
+              <Link
                 href="https://discord.gg/8R4aUFxp4j"
                 className="text-sm md:text-base opacity-50 mb-2"
                 target="_blank"
               >
                 Discord
-              </a>
-              <a
+              </Link>
+              <Link
                 href="https://asvalabs.medium.com/"
                 className="text-sm md:text-base opacity-50"
                 target="_blank"
               >
                 Medium
-              </a>
+              </Link>
             </div>
           </div>
         </div>
 
         <div className="flex justify-center items-center text-xs md:text-sm gap-1">
           Developed by{" "}
-          <a href="https://jayjeet-portfolio-tan.vercel.app/" target="_blank">
+          <Link
+            href="https://jayjeet-portfolio-tan.vercel.app/"
+            target="_blank"
+          >
             Jayjeet Kumar
-          </a>
+          </Link>
         </div>
       </div>
-
-    
     </div>
   );
 };
